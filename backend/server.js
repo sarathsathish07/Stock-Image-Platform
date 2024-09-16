7d1cf5e5655c73c1f3969e186ed92abbc47b1237
sarathsathish07 <sarathsathish1998@gmail.com>
2024-09-16 20:11:27 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 import { notFound,errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
