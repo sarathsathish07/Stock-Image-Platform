aa291a88e1d903c8c6f998eef573d30cd38ffc1b
sarathsathish07 <sarathsathish1998@gmail.com>
2024-09-16 20:16:18 +0530

    token

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://stock-image-platform.onrender.com',
+  credentials: 'include', 
 });
 
 export const apiSlice = createApi({
