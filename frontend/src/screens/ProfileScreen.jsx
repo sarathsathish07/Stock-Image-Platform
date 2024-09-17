db06ae14165addff96116ccc3397a942157dec45
sarathsathish07 <sarathsathish1998@gmail.com>
2024-09-17 12:09:41 +0530

    profile update

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,7 +7,6 @@ import Loader from "../components/Loader";
 import { setCredentials } from "../slices/authSlice";
 import { useNavigate } from "react-router-dom";
 import { useUpdateUserMutation } from "../slices/usersApiSlice";
-const PROFILE_IMAGE_DIR_PATH = 'http://localhost:5000/UserProfileImages/'
 
 
 const ProfileScreen = () => {
@@ -16,7 +14,6 @@ const ProfileScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [profileImageName, setProfileImageName] = useState("");
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -25,8 +22,7 @@ const ProfileScreen = () => {
   useEffect(() => {
     setName(userInfo.name);
     setEmail(userInfo.email);
-    setProfileImageName(userInfo.profileImageName);
-  }, [userInfo.setName, userInfo.setEmail,userInfo.profileImageName]);
+  }, [userInfo.setName, userInfo.setEmail]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -49,7 +45,6 @@ const ProfileScreen = () => {
       formData.append('name', name);
       formData.append('email', email);
       formData.append('password', password);
-      formData.append('profileImageName', profileImageName);
   const responseFromApiCall = await updateProfile(formData).unwrap();
   dispatch(setCredentials({...responseFromApiCall}))
   toast.success('Profile Updated Succesfully')
@@ -62,23 +57,6 @@ const ProfileScreen = () => {
 
   return (
     <FormContainer>
-       {userInfo.profileImageName && (
-        <img
-          src={PROFILE_IMAGE_DIR_PATH + userInfo.profileImageName}
-          alt={userInfo.name}
-          style={{
-            width: "150px",
-            justifyContent:"center",
-            height: "150px",
-            borderRadius: "50%",
-            objectFit: "cover",
-            display: "block",
-            marginTop: "5px",
-            marginLeft: "115px",
-            marginBottom: "10px",
-          }}
-        />
-      )}
       <h1>Update Profile</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className="my-2" controlId="name">
@@ -119,14 +97,6 @@ const ProfileScreen = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-
-        <Form.Group className="my-2" controlId="profileImage">
-              <Form.Label>Profile Picture</Form.Label>
-              <Form.Control
-                type="file"
-                onChange={(e) => setProfileImageName(e.target.files[0])}
-              ></Form.Control>
-            </Form.Group>
 
         {isLoading && <Loader/>}
 
